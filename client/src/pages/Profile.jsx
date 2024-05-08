fbd16d560f1ec38b30ff217e7d8c1cc7fd4e1ec0
Kevin sanders <ksanders8413@yahoo.com>
2024-05-08 09:46:08 -0500

    complete delete user listing functionality

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -138,6 +138,23 @@ export default function Profile() {
     }
   };
 
+  const handleListingDelete = async (listingId) => {
+    try {
+      const res = await fetch(`/api/listing/delete/${listingId}`, {
+        method: 'DELETE',
+
+      })
+      const data = await res.json()
+      if(data.success === false){
+        console.log(data.message)
+        return
+      }
+      setUserListings((prev) => prev.filter((listing) => listing._id !== listingId))
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className="taxt-3xl font-semibold text-center my-7">Profile</h1>
